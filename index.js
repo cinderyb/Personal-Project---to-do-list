482dda9a5b18ff3df46a101cbb563fb437dbd138
cinderyb <105159391+cinderyb@users.noreply.github.com>
2022-10-04 22:21:33 +0800

    aligned check boxes and submit button

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $("button").click(function () {
 
   var taskInput= $("#taskInput");
   var newTask = taskInput.val();
-  $("ul").append(check + "<li>" + (taskNumber + 1) + ". " + newTask + "</li>");
+  $("ul").append("<li>"+ (taskNumber + 1) + ". " + newTask + check + "</li>");
   taskNumber++;
   $("input").val('');
 
