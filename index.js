da1dba18e393703b37bb6342aa9eb902c1ac4432
cinderyb <105159391+cinderyb@users.noreply.github.com>
2022-10-04 10:58:37 +0800

    Added checkbox and enter key

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// let todoItems = [];
-//
-// function addItem(text){
-//   const todo= {
-//     text,
-//     checked:false,
-//     id: Date.now(),
-//   };
-//   todoItems.push(todo);
-//   console.log(todoItems);
-// }
 var taskNumber = 0;
+var check = '<input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">';
 
-const submitButton = $("button").click(function(){
-  var newTask = ($("#taskInput").val());
-  $("ul").append("<li>" + (taskNumber+1) + ". "+newTask + "</li>")
+
+$("button").click(function () {
+
+  var taskInput= $("#taskInput");
+  var newTask = taskInput.val();
+  $("ul").append(check + "<li>" + (taskNumber + 1) + ". " + newTask + "</li>");
   taskNumber++;
+  $("input").val('');
+
 });
 
-var todoArray = [];
+$("#taskInput").keyup(function(event) {
+    if (event.keyCode === 13) {
+      $("button").click();
+    }
+  });
